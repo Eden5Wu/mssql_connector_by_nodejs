762641b705d59e38fee5da999d24f3a5f487e1f1
Eden5Wu <yrw565@yahoo.com.tw>
2025-04-09 14:16:31 +0800

    Update date_utils.js

--- date_utils.js
+++ date_utils.js
@@ -16,7 +16,10 @@ dayjs.prototype.formatISOWithTimezone = function () {
   const utcSeconds = this.utc().second();
   const utcMilliseconds = this.utc().millisecond();
   // utcOffset() 返回以分鐘為單位的時區偏移量
-  const offsetHours = this.utcOffset() / 60;
+  const offsetMin = this.utcOffset();
+  const offsetHours = String(Math.abs(Math.floor(offsetMin / 60))).padStart(2, '0');
+  const offsetMinutes = String(Math.abs(offsetMin % 60)).padStart(2, '0');
+  const offsetSign = offsetMin > 0 ? '+' : '-';
 
   let localDateStr = this.format('YYYY-MM-DD');
 
@@ -24,7 +27,7 @@ dayjs.prototype.formatISOWithTimezone = function () {
   if (utcHours === 0 && utcMinutes === 0 && utcSeconds === 0 && utcMilliseconds === 0) {
     return localDateStr; // 使用本地時區格式化日期
   } else {
-    return `${localDateStr}T${utcHours.toString().padStart(2, '0')}:${utcMinutes.toString().padStart(2, '0')}:${utcSeconds.toString().padStart(2, '0')}.${utcMilliseconds.toString().padStart(3, '0')}${(offsetHours < 0 ? '-' : '+')}${Math.abs(offsetHours).toString().padStart(2, '0')}:00`; // 添加 T 和 +08:00
+    return `${localDateStr}T${utcHours.toString().padStart(2, '0')}:${utcMinutes.toString().padStart(2, '0')}:${utcSeconds.toString().padStart(2, '0')}.${utcMilliseconds.toString().padStart(3, '0')}${offsetSign}${offsetHours}:${offsetMinutes}`;
   }
 };
 
